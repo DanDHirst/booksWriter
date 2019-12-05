eaec38b7b56fb4e34a8aad529341d50d74633590
mlbarker <mlbarker@DM00202>
2019-12-05 13:34:27 +0000

    test

---
+++ StreamWriter/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Saving
+{
+    class Book
+    {
+        String name;
+        String author;
+        int copies;
+
+        public Book (String name, String author, int copies)
+        {
+            this.name = name;
+            this.author = author;
+            this.copies = copies;
+        }
+
+        public String toString()
+        {
+            return String.Format("{0}\n{1}\n{2}", name, author, copies);
+        }
+    }
+}
---
+++ StreamWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Saving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList books = new ArrayList();
+
+            books.Add(new Book("The fellowship of the ring", "JRR Tolkein", 5));
+            books.Add(new Book("The final empire", "Brandon Sanderson", 2));
+
+            write("books.txt", books);
+            Console.WriteLine("Complete");
+        }
+
+        public static void write(String filePath, ArrayList items)
+        {
+            using (StreamWriter w = new StreamWriter(filePath))
+            {
+                foreach (Book book in items)
+                {
+                    w.Write(book.toString());
+                    w.Write("\n");
+                }
+            }
+        }
+    }
+}
